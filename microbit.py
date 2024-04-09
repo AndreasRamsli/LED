3c72b4a543b795c100bab2760859360fe908e5b1
andreas <aramsli280194@gmail.com>
2024-04-09 19:45:41 +0200

    displaying 'unknown' state when disconnected

--- microbit.py
+++ microbit.py
@@ -14,48 +14,32 @@ class MicrobitDisplay:
         self.valid_from = "00:00"
         self.total_price = "0.0"
         uart.init(baudrate=115200)
-    
-    def check_for_interrupt(self):
-        return uart.any() or button_a.is_pressed() or button_b.is_pressed()
 
     def animate_price_level(self):
         sequence = self.animation_sequences.get(self.price_level, [(Image.CONFUSED, 1000)])
-        while not self.check_for_interrupt():
-            for image, delay in sequence:
-                display.show(image)
-                sleep(delay)
-                if self.check_for_interrupt():
-                    return
-    
+        for image, delay in sequence:
+            display.show(image)
+            sleep(delay)
+
     def update_display(self):
         if uart.any():
-            try:
-                data = uart.readline().decode('utf-8').rstrip()
-                new_price_level, self.valid_from, self.total_price = data.split(',')
-                display.clear()
-                
-                old_level_priority = self.get_price_level_priority(self.price_level)
-                new_level_priority = self.get_price_level_priority(new_price_level)
-                
-                if new_price_level != self.price_level:
-                    # Determine the direction of the price level change
-                    if new_level_priority > old_level_priority:
-                        # Moving up: Play a rising tone
-                        music.play(music.POWER_UP)
-                    elif new_level_priority < old_level_priority:
-                        # Moving down: Play a falling tone
-                        music.play(music.POWER_DOWN)
-                    else:
-                        # No change in level or unknown, optional: play a neutral sound
-                        pass
-                    
-                    self.price_level = new_price_level
+            data = uart.readline().decode('utf-8').rstrip()
+            if data == "TERMINATE":
+                self.price_level = "Unknown"
+                self.valid_from = "00:00"
+                self.total_price = "0.0"
+                display.show(Image.CONFUSED)
+                return
 
+            try:
+                new_price_level, self.valid_from, self.total_price = data.split(',')
+                self.price_level = new_price_level
+                display.clear()
             except ValueError:
                 display.show(Image.SAD)
                 sleep(1000)
                 display.clear()
-                
+
         if button_a.is_pressed() and button_b.is_pressed():
             display.scroll("Time: " + self.valid_from + " Price: " + self.total_price, delay=100)
         elif button_a.is_pressed():
@@ -64,17 +48,6 @@ class MicrobitDisplay:
             display.scroll(self.total_price, delay=100)
         else:
             self.animate_price_level()
-            
-    def get_price_level_priority(self, price_level):
-        priority = {
-            'VERY_CHEAP': 0,
-            'CHEAP': 1,
-            'NORMAL': 2,
-            'EXPENSIVE': 3,
-            'VERY_EXPENSIVE': 4
-        }
-        return priority.get(price_level, -1)  # Unknown or any other level defaults to -1
-
 
 microbit_display = MicrobitDisplay()
 
